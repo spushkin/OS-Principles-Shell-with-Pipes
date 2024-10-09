645116abeff700365a519c053b9e332f2d9e2d33
spushkin <pushkin.s89@gmail.com>
2024-10-09 13:26:38 -0700

    Cleanup

--- Pushkin_Siarhei_HW3_main.c
+++ Pushkin_Siarhei_HW3_main.c
@@ -1,7 +1,6 @@
 /**************************************************************
 * Class::  CSC-415-01 Summer 2024
 * Name:: Siarhei Pushkin
-* Student ID:: 922907437
 * GitHub-Name:: spushkin
 * Project:: Assignment 3 – Simple Shell with Pipes
 *
